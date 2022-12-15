b767ddbdf07c577ede599be8d8f49fdd7bad7dc6
NameIsNick <woosoungcho@gmail.com>
2022-12-15 20:07:03 +0900

    build : update default menubar

--- toss/src/App.js
+++ toss/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
-import Logo from "../src/assets/toss_logo.svg"
+import Logo from "../src/assets/images/toss_logo.svg"
+import Mainimage from "../src/assets/images/main-background-image.png";
 
 function App() {
   return (
@@ -8,7 +9,6 @@ function App() {
       <nav className = "navbar">
         <div className = "navbar_default">
           <div className = "navbar_inner">
-
             <div className = "logo_container">
               <a className = "logo" href = "/">
                 <span className = "icon">
@@ -16,7 +16,6 @@ function App() {
                 </span>
               </a> 
             </div>
-            
             <div className = "navbar_content">
               <ul className = "navbar-menu">
                 <li className = "navbar-item">
@@ -37,13 +36,20 @@ function App() {
                 <div className = "language">
                   <li className = "navbar-item"><a className = "korean">KOR</a></li>
                   <span className = "shortgray">|</span>
-                  <li className = "navbar-item"><a>ENG</a></li>
+                  <li className = "navbar-item"><a className = "english">ENG</a></li>
                 </div>
               </ul>
             </div>
           </div>
         </div>
       </nav>
+      {/* <section className = "main-page">
+        <div className = "main-section">
+          <div className = "main-background-image">
+            <img src = {Mainimage} className = "main-image"></img>
+          </div>
+        </div>
+      </section> */}
     </div>
   );
 }
